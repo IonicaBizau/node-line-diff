721bfc369ac3037de466404003df4446e556a7f1
Ionică Bizău <bizauionica@gmail.com>
2018-01-21 12:50:26 +0200

    Fix #11. Migrate to ES6 and fix functionality

--- example/index.js
+++ example/index.js
@@ -1,20 +1,31 @@
-// Dependencies
-var LineDiff = require("../lib")
-  , Fs = require("fs")
-  ;
+const LineDiff = require("../lib")
+    , fs = require("fs")
 
 // Show differences between the two files
 console.log(
-    LineDiff(
-        Fs.readFileSync(__dirname + "/1.txt", "utf-8")
-      , Fs.readFileSync(__dirname + "/2.txt", "utf-8")
+    new LineDiff(
+        fs.readFileSync(__dirname + "/1.txt", "utf-8")
+      , fs.readFileSync(__dirname + "/2.txt", "utf-8")
     ).toString()
-);
+)
+//   not modified
+// - old
+// - deleted
+// -
+// - 12345
+// + new
+// + 1234
 
 console.log(
-    LineDiff(
-        Fs.readFileSync(__dirname + "/1.txt", "utf-8")
-      , Fs.readFileSync(__dirname + "/2.txt", "utf-8")
+    new LineDiff(
+        fs.readFileSync(__dirname + "/1.txt", "utf-8")
+      , fs.readFileSync(__dirname + "/2.txt", "utf-8")
       , 3
     ).toString()
-);
+)
+//   not modified
+//   new
+// - deleted
+// -
+// - 12345
+// + 1234
